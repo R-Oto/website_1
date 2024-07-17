b564075373cde08d08016f73881d839c8833b903
R.Oto <r.obusinesscommercial@gmail.com>
2024-07-17 15:17:49 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper-container', {
+       loop: true,
+       pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+       },
+       navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+       },
+       autoplay: {
+          delay: 5000, // Set a reasonable delay
+          disableOnInteraction: false,
+       },
+       effect: 'slide',
+       speed: 900,
+    });
+ });
