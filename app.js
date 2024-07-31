9bb536f971c8320bd419d440d8baa9938f7d1896
R.Oto <r.obusinesscommercial@gmail.com>
2024-07-31 14:55:40 -0700

    part 1

--- app.js
+++ app.js
@@ -37,3 +37,22 @@ function scrollFunction() {
 scrollToTopBtn.addEventListener("click", function() {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+/* Menu hidden */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
